4575d87861c757e48904a1e5d4c59744b2623b07
icebear2n2 <icebear2n2@gmail.com>
2023-09-13 15:47:42 +0900

    FEAT: 메인 페이지 구현

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,156 +1,370 @@
+import ZoomableImage from "../components/ZoomableImage";
+
 const MainPage = () => {
   return (
     <>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
-      <h1>test</h1>
+      <div
+        className="mainGallery"
+        style={{
+          width: "1256px",
+          height: "508px",
+          margin: "0 120px 0 120px",
+          padding: "0 60px 0 60px",
+          marginTop: "31px",
+        }}
+      >
+        <ZoomableImage
+          src="https://github.com/icebear2n2/FE-Community-Service/assets/87232411/b4acc24a-8d09-4980-abe0-35b519500eb4"
+          alt="main-photo"
+          style={{}}
+        />
+        <div
+          className="main-photo-2"
+          style={{
+            width: "269px",
+            height: "508px",
+            border: "1px solid #424242",
+            borderRadius: "4px",
+            float: "left",
+          }}
+        ></div>
+      </div>
+      <div
+        className="icon-list"
+        style={{
+          width: "1256px",
+          height: "106px",
+          margin: "0 120px 0 120px",
+          padding: "0 60px 0 60px",
+          textAlign: "center",
+        }}
+      >
+        <div style={{ width: "113px", height: "106px", float: "left" }}>
+          <img
+            width="103px"
+            height="90px"
+            src="https://github.com/icebear2n2/FE-Community-Service/assets/87232411/32897504-d79a-400d-a328-61e25b6d33db"
+            alt=""
+            style={{ marginRight: "26px" }}
+          />
+          <p
+            style={{
+              fontSize: "16px",
+              letterSpacing: "-0.3px",
+              lineHeight: "20px",
+              textAlign: "center",
+            }}
+          >
+            쇼핑하기
+          </p>
+        </div>
+        <div style={{ width: "113px", height: "106px", float: "left" }}>
+          <img
+            width="103px"
+            height="90px"
+            src="https://github.com/icebear2n2/FE-Community-Service/assets/87232411/d1887532-36b6-4d78-b052-1aa15feddb71"
+            alt=""
+            style={{ marginRight: "26px" }}
+          />
+          <p
+            style={{
+              fontSize: "16px",
+              letterSpacing: "-0.3px",
+              lineHeight: "20px",
+              textAlign: "center",
+            }}
+          >
+            리빙페어
+          </p>
+        </div>
+
+        <div style={{ width: "113px", height: "106px", float: "left" }}>
+          <img
+            width="103px"
+            height="90px"
+            src="https://github.com/icebear2n2/FE-Community-Service/assets/87232411/049b356d-325c-4406-bd8a-b57b13f41664"
+            alt=""
+            style={{ marginRight: "26px" }}
+          />
+          <p
+            style={{
+              fontSize: "16px",
+              letterSpacing: "-0.3px",
+              lineHeight: "20px",
+              textAlign: "center",
+              color: "#2f3438",
+            }}
+          >
+            오늘의딜
+          </p>
+        </div>
+        <div style={{ width: "113px", height: "106px", float: "left" }}>
+          <img
+            width="103px"
+            height="90px"
+            src="https://github.com/icebear2n2/FE-Community-Service/assets/87232411/c78b25bd-461a-4aa1-bec1-d15f806b26d9"
+            alt=""
+          />
+          <p
+            style={{
+              fontSize: "16px",
+              letterSpacing: "-0.3px",
+              lineHeight: "20px",
+              textAlign: "center",
+              color: "#2f3438",
+            }}
+          >
+            오늘의발견
+          </p>
+        </div>
+        <div style={{ width: "113px", height: "106px", float: "left" }}>
+          <img
+            width="103px"
+            height="90px"
+            src="https://github.com/icebear2n2/FE-Community-Service/assets/87232411/dc31a627-dcc5-4b2f-9997-23c8e6f5a68c"
+            alt=""
+            style={{ marginRight: "26px" }}
+          />
+          <p
+            style={{
+              fontSize: "16px",
+              letterSpacing: "-0.3px",
+              lineHeight: "20px",
+              textAlign: "center",
+              color: "#2f3438",
+            }}
+          >
+            장보기
+          </p>
+        </div>
+        <div style={{ width: "113px", height: "106px", float: "left" }}>
+          <img
+            width="103px"
+            height="90px"
+            src="https://github.com/icebear2n2/FE-Community-Service/assets/87232411/36f94168-c14f-4424-8a7c-d89468124542"
+            alt=""
+          />
+          <p
+            style={{
+              fontSize: "16px",
+              letterSpacing: "-0.3px",
+              lineHeight: "20px",
+              textAlign: "center",
+              color: "#2f3438",
+            }}
+          >
+            집들이
+          </p>
+        </div>
+        <div style={{ width: "113px", height: "106px", float: "left" }}>
+          <img
+            width="103px"
+            height="90px"
+            src="https://github.com/icebear2n2/FE-Community-Service/assets/87232411/23032600-b729-4cae-a762-f0151dc511ec"
+            alt=""
+            style={{ marginRight: "26px" }}
+          />
+          <p
+            style={{
+              fontSize: "16px",
+              letterSpacing: "-0.3px",
+              lineHeight: "20px",
+              textAlign: "center",
+              color: "#2f3438",
+            }}
+          >
+            관심채널
+          </p>
+        </div>
+        <div style={{ width: "113px", height: "106px", float: "left" }}>
+          <img
+            width="103px"
+            height="90px"
+            src="https://github.com/icebear2n2/FE-Community-Service/assets/87232411/36d59cbf-9fe6-4ff7-a054-ee69314dfa3d"
+            alt=""
+          />
+          <p
+            style={{
+              fontSize: "16px",
+              letterSpacing: "-0.3px",
+              lineHeight: "20px",
+              textAlign: "center",
+              color: "#2f3438",
+            }}
+          >
+            빠른배송
+          </p>
+        </div>
+
+        <div style={{ width: "113px", height: "10링6px", float: "left" }}>
+          <img
+            width="103px"
+            height="90px"
+            src="https://github.com/icebear2n2/FE-Community-Service/assets/87232411/991ab6a9-b75a-4019-bc7e-b7333536ae18"
+            alt=""
+          />
+          <p
+            style={{
+              fontSize: "16px",
+              letterSpacing: "-0.3px",
+              lineHeight: "20px",
+              textAlign: "center",
+              color: "#2f3438",
+            }}
+          >
+            리모델링
+          </p>
+        </div>
+
+        <div style={{ width: "113px", height: "106px", float: "left" }}>
+          <img
+            width="103px"
+            height="90px"
+            src="https://github.com/icebear2n2/FE-Community-Service/assets/87232411/789e4b8f-52a5-4b14-b72d-4c22ecc851aa"
+            alt=""
+            style={{ marginRight: "26px" }}
+          />
+          <p
+            style={{
+              fontSize: "16px",
+              letterSpacing: "-0.3px",
+              lineHeight: "20px",
+              textAlign: "center",
+              color: "#2f3438",
+            }}
+          >
+            쉬운이사
+          </p>
+        </div>
+      </div>
+      <div
+        className="house-tour-posts"
+        style={{
+          width: "1256px",
+          height: "305px",
+          margin: "40px 120px 40px 120px",
+          padding: "0 60px 0 60px",
+        }}
+      >
+        <div
+          style={{ width: "1236px", height: "28px", padding: "20px 0 20px 0" }}
+        >
+          <div
+            style={{
+              width: "1084px",
+              height: "28px",
+              fontSize: "20px",
+              lineHeight: "28px",
+              letterSpacing: "-0.3px",
+              color: "#2f3438",
+              textAlign: "start",
+              fontWeight: "700",
+              float: "left",
+            }}
+          >
+            <p>40평 이상 best 집들이 모았어요 👑</p>
+          </div>
+          <div
+            style={{
+              width: "41px",
+              height: "20px",
+              fontSize: "18px",
+              lineHeight: "20px",
+              textAlign: "center",
+              color: "#35c5f0",
+              fontWeight: "700",
+              float: "left",
+              whiteSpace: "nowrap",
+            }}
+          >
+            <p>더보기</p>
+          </div>
+        </div>
+        <div
+          style={{
+            width: "269px",
+            height: "237px",
+            fontSize: "16px",
+            lineHeight: "20px",
+            textAlign: "start",
+            letterSpacing: "-0.3px",
+            marginTop: "20px",
+            marginRight: "20px",
+            float: "left",
+          }}
+        >
+          <img
+            width="269px"
+            src="https://github.com/icebear2n2/FE-Community-Service/assets/87232411/b4acc24a-8d09-4980-abe0-35b519500eb4"
+            alt="main-photo"
+            style={{ borderRadius: "4px", marginBottom: "10px" }}
+          />
+          <p>아이와 반려견이 함께 뛰노는 싱그러운 42평 저층 아파트</p>
+        </div>
+        <div
+          style={{
+            width: "269px",
+            height: "237px",
+            fontSize: "16px",
+            lineHeight: "20px",
+            textAlign: "start",
+            letterSpacing: "-0.3px",
+            marginTop: "20px",
+            marginRight: "20px",
+            float: "left",
+          }}
+        >
+          <img
+            width="269px"
+            src="https://github.com/icebear2n2/FE-Community-Service/assets/87232411/b4acc24a-8d09-4980-abe0-35b519500eb4"
+            alt="main-photo"
+            style={{ borderRadius: "4px", marginBottom: "10px" }}
+          />
+          <p>아이와 반려견이 함께 뛰노는 싱그러운 42평 저층 아파트</p>
+        </div>
+        <div
+          style={{
+            width: "269px",
+            height: "237px",
+            fontSize: "16px",
+            lineHeight: "20px",
+            textAlign: "start",
+            letterSpacing: "-0.3px",
+            marginTop: "20px",
+            marginRight: "20px",
+            float: "left",
+          }}
+        >
+          <img
+            width="269px"
+            src="https://github.com/icebear2n2/FE-Community-Service/assets/87232411/b4acc24a-8d09-4980-abe0-35b519500eb4"
+            alt="main-photo"
+            style={{ borderRadius: "4px", marginBottom: "10px" }}
+          />
+          <p>아이와 반려견이 함께 뛰노는 싱그러운 42평 저층 아파트</p>
+        </div>
+        <div
+          style={{
+            width: "269px",
+            height: "237px",
+            fontSize: "16px",
+            lineHeight: "20px",
+            textAlign: "start",
+            letterSpacing: "-0.3px",
+            marginTop: "20px",
+
+            float: "left",
+          }}
+        >
+          <img
+            width="269px"
+            src="https://github.com/icebear2n2/FE-Community-Service/assets/87232411/b4acc24a-8d09-4980-abe0-35b519500eb4"
+            alt="main-photo"
+            style={{ borderRadius: "4px", marginBottom: "10px" }}
+          />
+          <p>아이와 반려견이 함께 뛰노는 싱그러운 42평 저층 아파트</p>
+        </div>
+      </div>
     </>
   );
 };
