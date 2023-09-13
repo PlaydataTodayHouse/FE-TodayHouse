cf19531c8609ebcd41937be1c194e27d5c442311
icebear2n2 <icebear2n2@gmail.com>
2023-09-13 17:27:49 +0900

    FEAT: 사진/동영상 게시물 작성 페이지 렌더링 구현 중

--- src/routes/MyRoutes.jsx
+++ src/routes/MyRoutes.jsx
@@ -1,15 +1,20 @@
-import {Route, Routes} from "react-router";
+import { Route, Routes } from "react-router";
 import Template from "../templates/Template";
 import MainPage from "../pages/Main";
+import MediaPost from "../pages/MediaPost";
+import MediaPostTemplate from "../templates/MediaPostTemplate";
 
 const MyRoutes = () => {
-    return (
-        <Routes>
-            <Route element={<Template />}>
-                <Route path='/' element={<MainPage />}/>
-            </Route>
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route element={<Template />}>
+        <Route path="/" element={<MainPage />} />
+      </Route>
+      <Route element={<MediaPostTemplate />}>
+        <Route path="/media-post" element={<MediaPost />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default MyRoutes;
