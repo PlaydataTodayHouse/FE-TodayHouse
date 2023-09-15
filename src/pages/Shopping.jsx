fcb5d73593ccfe4e1de25be49701768ec3622b87
JUNGI JEONG <junejjk@gmail.com>
2023-09-15 15:03:16 +0900

    image test

--- src/pages/Shopping.jsx
+++ src/pages/Shopping.jsx
@@ -1,6 +1,11 @@
+import ShoppingCarousel from "../components/ShoppingCarousel";
 
 const ShoppingPage = () => {
-
-}
-
+    return (
+        <div>
+          <h1>Shopping Page</h1>
+          <ShoppingCarousel />
+        </div>
+      );
+    }
 export default ShoppingPage;
