aab0c7c7ced10f388ec508d36b841bd0692a31e3
JUNGI JEONG <junejjk@gmail.com>
2023-09-17 15:54:04 +0900

    logo image

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -58,23 +58,22 @@ export default function Login() {
                       style={{ width: '70%', height: '10%' }}
                   />
                   <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                      Sign in to your account
                   </h2>
               </div>
-              {/* 이메일 */}
+              {/* 아이디 */}
               <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                   <form className="space-y-6" onSubmit={onSubmitHandler}>
                       <div>
                           <div className="flex items-center justify-between">
-                              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                              <label htmlFor="userId" className="block text-sm font-medium leading-6 text-gray-900">
                                   Email address
                               </label>
                           </div>
                           <div className="mt-2">
                               <input
-                                  id="email"
-                                  name="email"
-                                  type="email"
+                                  id="userId"
+                                  name="userId"
+                                  type="userId"
                                   onChange={onChangeHandler}
                                   required
                                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 tracking-wider"
